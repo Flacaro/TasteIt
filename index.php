ffdf528351651f3d2ba73f7148a87cfb84fbe715
Flacaro <flavianacaroselli@gmail.com>
2021-04-11 19:06:34 +0200

    added routes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-/* require_once "vendor/autoload.php";
+ require_once "vendor/autoload.php";
 require_once "routes.php";
 
 use Pecee\SimpleRouter\SimpleRouter;
@@ -8,6 +8,4 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::setDefaultNamespace("\App\Controllers");
 
-SimpleRouter::start(); */
-
-echo "CIAO";
+SimpleRouter::start(); 
