6a1b57d2e4fe56c6daa830a40b053902105896f8
Flacaro <flavianacaroselli@gmail.com>
2021-08-01 21:22:56 +0200

    test smarty

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -6,8 +6,9 @@ namespace App\Controllers;
 class TodoController {
 
    public function index() {
-       $smarty = $GLOBALS['smarty'];
-       $smarty->assign('name', 'Flavy');
-       return $smarty->display('src/templates/product.tpl');
+    //    $smarty = $GLOBALS['smarty'];
+    //    $smarty->assign('name', 'Flavy');
+    //    return $smarty->display('src/templates/product.tpl');
+    echo "<h1> Ciao </h1>";
    }
 }
