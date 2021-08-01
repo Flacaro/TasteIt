4d56cf24d06631c44cdf30eebc9b8a07ee1a3a21
Flacaro <flavianacaroselli@gmail.com>
2021-08-01 21:21:51 +0200

    test smarty

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -3,12 +3,9 @@
 namespace App\Controllers;
 
 
-use App\Views\Todos;
-
 class TodoController {
 
    public function index() {
-       $todosView = new Todos();
        $smarty = $GLOBALS['smarty'];
        $smarty->assign('name', 'Flavy');
        return $smarty->display('src/templates/product.tpl');
