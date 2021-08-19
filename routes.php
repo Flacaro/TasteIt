9037f3fc748e27cfd7f42b7e7c17b9b0dda315af
MohamedOssama24 <81478211+MohamedOssama24@users.noreply.github.com>
2021-08-19 16:00:54 +0200

    interfaccia

--- routes.php
+++ routes.php
@@ -15,14 +15,14 @@ use Pecee\SimpleRouter\SimpleRouter;
 SimpleRouter::redirect("/", "/home");
 
 SimpleRouter::get("/home", [HomePageController::class, "visualizeHome"]);
-<<<<<<< HEAD
+
 SimpleRouter::get("/products", [ProductController::class, "index"])->name('products');
 SimpleRouter::get("productsingle", [ProductController::class, "visualizzasingolo"]);
 SimpleRouter::get("/aboutUs", [HomePageController::class, "About"]);
 
-=======
+
 SimpleRouter::get("/products", [ProductController::class, "index"]);
->>>>>>> 8354e198789bfae134450d04d77a32037b327e0b
+
 SimpleRouter::get("/products/create", [ProductController::class, "create"]);
 SimpleRouter::get("/users", [UserController::class, "index"]);
 //Il primo router serve per visualizzare il form, il secondo per andare a prendere i dati dal db per fare l'update
